eee18936813bb3cf2497e13abceb810232daed05
Massau <joyce.santos202@etec.sp.gov.br>
2019-04-15 02:34:35 -0300

    Valida teste de duração

--- Model/Filme.php
+++ Model/Filme.php
@@ -4,7 +4,8 @@ App::uses('AppModel', 'Model');
 class Filme extends AppModel {
     
     public $validate = array(
-        'nome' => array('rule' => 'notEmpty', 'message' => 'Informe o nome')
+        'nome' => array('rule' => 'notBlank', 'message' => 'Informe o nome'),
+        'duracao' => array('rule' => 'notBlank', 'message' => 'Informe a duração'),
     );
 
 }
