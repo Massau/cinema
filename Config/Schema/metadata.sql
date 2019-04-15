78b3c1f97a283b7533944585d58e954c94f7ccd6
Massau <joyce.santos202@etec.sp.gov.br>
2019-04-15 03:04:06 -0300

    Insere registros da tabela Gêneros

--- Config/Schema/metadata.sql
+++ Config/Schema/metadata.sql
@@ -12,3 +12,17 @@ INSERT INTO filmes (id, nome, ano, duracao, idioma) VALUES ('2', 'Rocky', '1980'
 INSERT INTO filmes (id, nome, ano, duracao, idioma) VALUES ('3', 'De Volta para o Futuro', '1986', '3:00', 'Inglês');
 INSERT INTO filmes (id, nome, ano, duracao, idioma) VALUES ('4', 'Esqueceram de Mim', '1994', '3:00', 'Inglês');
 INSERT INTO filmes (id, nome, ano, duracao, idioma) VALUES ('5', 'Star Wars', '1977', '2:00', 'Inglês');
+
+CREATE TABLE `generos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO generos (id, nome) VALUES ('1', 'Aventura');
+INSERT INTO generos (id, nome) VALUES ('2', 'Ação');
+INSERT INTO generos (id, nome) VALUES ('3', 'Suspense');
+INSERT INTO generos (id, nome) VALUES ('4', 'Terror');
+INSERT INTO generos (id, nome) VALUES ('5', 'Ficção');
+INSERT INTO generos (id, nome) VALUES ('6', 'Comédia');
+INSERT INTO generos (id, nome) VALUES ('7', 'Drama');
