3944776fab3af60d4cb0b1d43c12f5e79744df02
Massau <joyce.santos202@etec.sp.gov.br>
2019-04-15 03:05:59 -0300

    Utiliza BelongsTo no modelo Filme

--- Model/Filme.php
+++ Model/Filme.php
@@ -3,6 +3,9 @@ App::uses('AppModel', 'Model');
 
 class Filme extends AppModel {
     
+    public $belongsTo = array(
+        'Genero'
+    );
     public $validate = array(
         'nome' => array('rule' => 'notBlank', 'message' => 'Informe o nome'),
         'duracao' => array('rule' => 'notBlank', 'message' => 'Informe a duração'),
