90e58d11d7878d92de5e145936f74695a0e889f0
Massau <joyce.santos202@etec.sp.gov.br>
2019-04-16 18:11:00 -0300

    Impossibilita não preencher campos no crud Ator

--- Model/Ator.php
+++ Model/Ator.php
@@ -3,4 +3,15 @@ App::uses('AppModel', 'Model');
 
 class Ator extends AppModel {
 
+    public $validate = array(
+        'nome' => array(
+            'rule' => 
+                'notBlank', 'message' => 'Informe o nome',
+                'minLength' => 3
+            ),
+        'nascimento' => array(
+            'rule' => 
+            'notBlank', 'message' => 'Informe a data de nascimento'),
+    );
+
 }
