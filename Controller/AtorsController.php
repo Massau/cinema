8f1545790350235986910cc0b3841a2752ad703e
Massau <joyce.santos202@etec.sp.gov.br>
2019-04-16 12:00:09 -0300

    Acessa dados do banco Ator e exibe dinamicamente

--- Controller/AtorsController.php
+++ Controller/AtorsController.php
@@ -5,13 +5,7 @@ class AtorsController extends AppController {
     
     public function index() {
     
-        //Como vem informação do modelo - Matriz tripla
-        $ators = array(
-            //    Model       'propriedade'
-            array('Ator' => array('nome' => 'Robert', 'nascimento' => '1980-03-02')),
-            array('Ator' => array('nome' => 'Claudia', 'nascimento' => '1945-04-04')),
-        );
-        //    Faz informação ficar visivel na view
+        $ators = $this->Ator->find('all');
         $this->set('ators', $ators);
     }
 
